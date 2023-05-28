4ebf65c0a5dc8bc838e6156dce176b734b128a97
seulgi <sgsg9447@gmail.com>
2023-05-29 02:50:08 +0900

    feat : implement ProductListPage #5

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+type ProductValues = {
+  id: string;
+  productName: string;
+  price: number;
+  stock: number;
+  category: string;
+  color: string;
+  material: string;
+  size: string;
+  image: string;
+};
