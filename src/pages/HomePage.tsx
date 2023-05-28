47a4315c69f919fbb1f3f1b38e980d6c357490e5
seulgi <sgsg9447@gmail.com>
2023-05-29 03:39:18 +0900

    feat: implement HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Link to="/products">
+      <HeroImage
+        src="https://seulgi-product-images.s3.ap-northeast-2.amazonaws.com/Plugo_Banner_Website.jpg"
+        alt="Product Image"
+      />
+    </Link>
+  );
 };
 
 export default HomePage;
+
+const HeroImage = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 100%;
+`;
