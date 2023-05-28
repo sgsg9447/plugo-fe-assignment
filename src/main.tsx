7e29197046e43414a3b5a35efe4873a7a997a965
seulgi <sgsg9447@gmail.com>
2023-05-29 01:13:04 +0900

    feat: Add Cabin fonts and define global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "@/App.tsx";
+import GlobalStyle from "@/styles/GlobalStyles.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <>
+    <GlobalStyle />
+    <App />
+  </>
 );
